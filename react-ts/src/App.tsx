f01dbd81696139efe39575ab344f31e72e4d5e91
loretari <loretari@gmail.com>
2023-07-03 10:43:20 +0300

    init: add Removing a Todo

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -16,12 +16,19 @@ function App() {
 
     };
 
+    const removeTodoHandler = (todoId: string) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter(todo => todo.id !== todoId);
+        })
+    };
+
+
 
 
   return (
     <div>
         <NewTodo  onAddTodo={addTodoHandler}/>
-        <Todos items={ todos }/>
+        <Todos items={ todos } onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
